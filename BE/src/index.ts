a8a104be1b83a9e73cc426717d142fbc03c3450c
imrajwardhan <patilrajwardhan7@gmail.com>
2026-04-28 15:03:37 +0000

    initial implementation of argon2

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -3,21 +3,22 @@
 import "dotenv/config";
 import express from "express";
 import { userSchema } from "./schema/zSchema.js";
+import * as argon2 from "argon2";
 
 const app = express();
 app.use(express.json());
 
-app.post("/api/v1/signup", (req: Request, res: Response): Response => {
+app.post("/api/v1/signup", async (req: Request, res: Response): Response => {
   try {
     const userResult = userSchema.safeParse(req.body);
 
     //TODO: call db to check if user exists or not
-    if (userResult.success)
-      res.status(200).json({ message: "User signed up successfully" });
-    else
+    if (!userResult.success)
       res
         .status(411)
         .json({ message: "Error in user inputs", error: userResult.error });
+
+    const hashedPassword: string = await argon2.hash(userResult.data?.password);
   } catch (e) {
     res.status(400).json({
       message: "Something went wrong",
@@ -25,6 +26,8 @@ app.post("/api/v1/signup", (req: Request, res: Response): Response => {
     });
   }
 });
+
+//TODO: signin end point with argon2 verify
 
 app.listen(process.env.PORT, (e: Error | undefined): void => {
   if (e) console.error(`There is this following error\n${e}`);
