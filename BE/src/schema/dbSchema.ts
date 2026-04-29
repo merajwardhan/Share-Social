25fcae91fc1e1bb457f3f509344797dfe1e40f10
imrajwardhan <patilrajwardhan7@gmail.com>
2026-04-29 13:51:04 +0000

    changed the import style according to ESM

--- BE/src/schema/dbSchema.ts
+++ BE/src/schema/dbSchema.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
-import { IUser, IContent } from "../types/dbTypes.ts";
+import type { IUser, IContent } from "../types/dbTypes.js";
 
 //TODO:  Index the DB
 const userSchema = new Schema<IUser>({
@@ -10,6 +10,7 @@ const userSchema = new Schema<IUser>({
   email: { type: String, unique: true, required: true },
   content: [{ type: String, ref: "Content" }],
 });
+//TODO: Check if we have to always provide the required fields we ever have to update the document.
 
 const contentSchema = new Schema<IContent>({
   title: { String, required: true },
