58841c02f4f6fe0c64df01bc1e855595d71fe840
imrajwardhan <patilrajwardhan7@gmail.com>
2026-05-22 11:04:26 +0000

    feat: added the whole shareable api

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -290,6 +290,52 @@ app.delete("/api/v1/content/:content", JWT_Auth, async (req, res) => {
   }
 });
 
+app.get("/api/v1/brain/:username", async (req, res) => {
+  try {
+    const username: string = req.params.username;
+    if (!username)
+      return res.status(403).json({ message: "Usernamej not provided!" });
+
+    const user = await User.aggregate([
+      { $match: { username } },
+      {
+        $lookup: {
+          from: "contents",
+          localField: "content",
+          foreignField: "_id",
+          as: "populatedContent",
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          username: 1,
+          password: 0,
+          email: 0,
+          shareable: 0,
+          allPosts: "$populatedContent",
+        },
+      },
+    ]);
+
+    if (!user || user[0].shareable !== true)
+      return res.status(403).json({
+        message: "User not found or User's content is not shareable!",
+      });
+
+    return res.status(200).json({
+      message: "These are all the sharable documents",
+      username: user[0].username,
+      allPosts: user[0].allPosts,
+    });
+  } catch (e) {
+    console.error("Something went wrong in the /brain endpoint\nError = " + e);
+    res.status(403).json({
+      message: "Something went wrong while fetching the contents of brain!",
+    });
+  }
+});
+
 app.listen(process.env.PORT, (e: Error | undefined): void => {
   if (e) console.error(`There is this following error\n${e}`);
   else console.log("Server running on Port " + process.env.PORT);
