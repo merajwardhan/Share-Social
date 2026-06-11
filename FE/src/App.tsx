9a9f3450cee3b0464a5b624d545765c06e6e9073
imrajwardhan <patilrajwardhan7@gmail.com>
2026-06-11 15:02:45 +0000

    feat: added patterns and background lines to the ui

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,10 +1,26 @@
+import { ThemeToggle } from "./components/themeButton";
+import { BackgroundLines } from "./components/ui/background-lines";
+import { useTheme } from "next-themes";
 import { Patterns } from "./components/ui/patterns";
-//TODO: Use next themes to setup darkmode and light mode
+import { useState, useEffect } from "react";
 
 function App() {
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <>
-      <Patterns varient="Light"></Patterns>
+      <Patterns varient={resolvedTheme === "dark" ? "dark" : "light"}>
+        <BackgroundLines>
+          <ThemeToggle></ThemeToggle>
+        </BackgroundLines>
+      </Patterns>
     </>
   );
 }
