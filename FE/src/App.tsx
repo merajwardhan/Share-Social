fe2cf1e0108d993d1dcaee617caa5bf068a01d92
imrajwardhan <patilrajwardhan7@gmail.com>
2026-06-13 11:38:49 +0000

    feat: implemented Navbar

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -3,10 +3,22 @@ import { BackgroundLines } from "./components/ui/background-lines";
 import { useTheme } from "next-themes";
 import { Patterns } from "./components/ui/patterns";
 import { useState, useEffect } from "react";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  NavbarLogo,
+  NavbarButton,
+  MobileNav,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "./components/ui/resizable-navbar";
 
 function App() {
   const { resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     setMounted(true);
@@ -14,11 +26,66 @@ function App() {
 
   if (!mounted) return null;
 
+  const navLinks = [
+    { name: "Home", link: "#" },
+    { name: "Explore", link: "#" },
+    { name: "Features", link: "#" },
+  ];
+
   return (
     <>
       <Patterns varient={resolvedTheme === "dark" ? "dark" : "light"}>
         <BackgroundLines>
-          <ThemeToggle></ThemeToggle>
+          <Navbar>
+            {/* Desktop Layout */}
+            <NavBody>
+              <NavbarLogo />
+              <NavItems items={navLinks} />
+
+              <div className="flex items-center gap-2">
+                <ThemeToggle />
+                <NavbarButton variant="dark">Sign Up</NavbarButton>
+              </div>
+            </NavBody>
+
+            {/* Mobile Layout */}
+            <MobileNav>
+              <MobileNavHeader>
+                <div className="flex items-center gap-2">
+                  <NavbarLogo />
+                </div>
+
+                <div className="flex items-center gap-4">
+                  <ThemeToggle />
+                  <MobileNavToggle
+                    isOpen={isMobileMenuOpen}
+                    onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                  />
+                </div>
+              </MobileNavHeader>
+            </MobileNav>
+            <MobileNavMenu
+              isOpen={isMobileMenuOpen}
+              onClose={() => setIsMobileMenuOpen(false)}
+            >
+              <div className="flex flex-col gap-4 w-full">
+                {navLinks.map((item, idx) => (
+                  <a
+                    key={idx}
+                    href={item.link}
+                    className="text-lg font-medium text-foreground"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+                <NavbarButton variant="dark" className="w-full mt-4">
+                  Sign Up
+                </NavbarButton>
+              </div>
+            </MobileNavMenu>
+          </Navbar>
+
+          {/* REST OF THE PAGE CONTENT HERE  */}
         </BackgroundLines>
       </Patterns>
     </>
