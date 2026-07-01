6b3bc81d37451bdb683917571aef01ed9d74a045
imrajwardhan <patilrajwardhan7@gmail.com>
2026-07-01 15:20:20 +0000

    feat: added and implemented react router dom

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { SignUp } from "./pages/Signup";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        {/* Default fallback to all the invalid pages */}
+        <Route
+          path="*"
+          element={<div className="text-center pt-20">Page Not Found</div>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
