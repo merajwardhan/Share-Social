67bafdef4e5eb944a8f9ed7cd87d774034e4022b
imrajwardhan <patilrajwardhan7@gmail.com>
2026-08-19 06:48:56 +0000

    feat: added the signup function for user

--- FE/src/lib/auth.ts
+++ FE/src/lib/auth.ts
@@ -1,1 +1,36 @@
-//All the fetch functions would be here
+import { type UserSigninFormData } from "./zodSchema";
+
+export const SignupUser = async (data: UserSigninFormData) => {
+  try {
+    const response = await fetch("http://localhost:3000/api/v1/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      return {
+        success: false,
+        error:
+          responseData.message ||
+          "Failed to create your account (Manual Error)",
+      };
+    }
+
+    return {
+      success: true,
+      userData: responseData,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error:
+        `Something went wrong!, \nError: ${error}` ||
+        "Something went wrong! (Manual Error)",
+    };
+  }
+};
