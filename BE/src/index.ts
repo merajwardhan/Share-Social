8b36682175b8fc000fe937429830ec95adc27b2a
imrajwardhan <patilrajwardhan7@gmail.com>
2026-08-20 07:59:11 +0000

    feat: removed post logic from index.ts

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -12,6 +12,8 @@ import * as argon2 from "argon2";
 import { User, Content } from "./schema/dbSchema.js";
 import type { IUser, IContent } from "./types/dbTypes.js";
 import { dbConnect } from "./helper/dbConnect.js";
+//TODO: Remove the unnecessary imports
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -20,71 +22,7 @@ dbConnect();
 //TODO:deal with cors erros and revise about it while you are at it
 //TODO:make specific routes for all the endpoint rather than cluttering altogether
 
-app.post("/api/v1/signup", async (req, res) => {
-  try {
-    const userResult = userSchema.safeParse(req.body);
-
-    if (!userResult.success) {
-      return res
-        .status(411)
-        .json({ message: "Error in user inputs", error: userResult.error });
-    }
-
-    const userExists = await User.findOne({
-      $or: [
-        { username: userResult.data.username },
-        { email: userResult.data.email },
-      ],
-    } as QueryFilter<IUser>).exec();
-
-    if (userExists) {
-      if (
-        userExists.email === userResult.data.email &&
-        userExists.username === userResult.data.username
-      )
-        return res.status(409).json({
-          message: "User already exists with this email and username",
-        });
-
-      if (userExists.email === userResult.data.email)
-        return res
-          .status(409)
-          .json({ message: "User with this email already exists!" });
-
-      return res
-        .status(409)
-        .json({ message: "User with this username already exists!" });
-    }
-
-    const hashedPassword: string = await argon2.hash(userResult.data?.password);
-
-    await User.create({
-      name: userResult.data.name,
-      username: userResult.data.username,
-      password: hashedPassword,
-      email: userResult.data.email,
-    });
-
-    const JWT_SECRET: string | undefined = process.env.JWT_SECRET;
-    if (!JWT_SECRET) {
-      return res.status(403).json({
-        message:
-          "Error with sign in function at environment variable handling!",
-      });
-    }
-
-    const token = jwt.sign({ username: userExists.username }, JWT_SECRET, {
-      expiresIn: "7d",
-    });
-    //TODO: Provide user data so user can return to dashboard on frontend after signup
-    return res
-      .status(200)
-      .json({ message: "User signed up successfully!", token });
-  } catch (e) {
-    console.error("Signup endpoint error = " + e);
-    return res.status(500).json({ message: "Something went wrong" });
-  }
-});
+app.post("/api/v1/", authRouter);
 
 app.post("/api/v1/signin", async (req, res) => {
   try {
