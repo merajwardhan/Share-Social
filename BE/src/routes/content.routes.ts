847f7d970c3d312bbd2f0a5503d91601742f393e
imrajwardhan <patilrajwardhan7@gmail.com>
2026-08-20 14:15:21 +0000

    feat: added the remaining content routes to the router

--- BE/src/routes/content.routes.ts
+++ BE/src/routes/content.routes.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import express from "express";
 import { JWT_Auth } from "../middlewares/JWT_Auth.js";
 import { uploadContent } from "../controllers/upload.content.controllers.js";
+import { editContent } from "../controllers/edit.content.controllers.js";
+import { fetchContent } from "../controllers/fetch.content.controllers.js";
+import { deleteContent } from "../controllers/delete.content.controllers.js";
 
 const router = Router();
 const app = express();
@@ -9,5 +12,7 @@ const app = express();
 app.use(JWT_Auth);
 
 router.post("/content", uploadContent);
-
+router.patch("/content/:contentId", editContent);
+router.get("/content", fetchContent);
+router.delete("/content/:content", deleteContent);
 export default router;
